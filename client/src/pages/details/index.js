3ede72c7ce47ce64821529aceea1c759f21d5d32
mkumar3 <mkumar3@exponential.com>
2019-06-27 12:54:59 +0530

    details form

--- client/src/pages/details/index.js
+++ client/src/pages/details/index.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import { Container,Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Container,Col,Card, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export default class Details extends React.Component {
   constructor(props){
@@ -9,38 +9,71 @@ export default class Details extends React.Component {
     console.log(props.location);
   }
   render() {
+    var stateValue = this.props.location.state;
     return (
       <Container>
+      <h1> Details Form </h1>
        <Form>
         <FormGroup row>
           <Label for="exampleEmail" sm={2}>Email</Label>
           <Col sm={10}>
-            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+            <Input type="email" name="email" id="exampleEmail"  value={stateValue.email}/>
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="NAME" sm={2}>NAME</Label>
+          <Col sm={10}>
+            <Input type="TEXT" name="name" id="name"   value={stateValue.name}/>
           </Col>
         </FormGroup>
        
         <FormGroup row>
-          <Label for="exampleSelect" sm={2}>Select</Label>
+          <Label for="Company" sm={2}>Company</Label>
           <Col sm={10}>
-            <Input type="select" name="select" id="exampleSelect" />
+            <Input type="select" name="Company" id="Company" >
+             <option>Atlassian</option>
+            <option>Shell</option>
+            <option>HoneyWell</option>
+            <option>Accenture</option>
+            <option>Inmobi</option>
+            </Input>
+          </Col>
+        </FormGroup>
+
+        <FormGroup row>
+          <Label for="Time Slot" sm={2}>Time Slot</Label>
+          <Col sm={10}>
+            <Input type="select" name="Time Slot" id="Time_Slot" >
+             <option>04.00PM</option>
+            <option>04.10PM</option>
+            <option>04.20PM</option>
+            <option>04.30PM</option>
+            <option>04.40PM</option>
+            </Input>
           </Col>
         </FormGroup>
         <FormGroup row>
-          <Label for="exampleFile" sm={2}>File</Label>
+          <Label for="Location" sm={2}>Location</Label>
           <Col sm={10}>
-            <Input type="file" name="file" id="exampleFile" />
-            <FormText color="muted">
-              This is some placeholder block-level help text for the above input.
-              It's a bit lighter and easily wraps to a new line.
-            </FormText>
+              <button id = "find-me">Show my location</button><br/>
           </Col>
         </FormGroup>
+        <FormGroup check>
+        <Col sm={10}>
+          <Label check>
+             <Input type="radio" /> Notify me for Alerts
+             
+          </Label>
+          </Col>
+        </FormGroup>
+      
         <FormGroup check row>
           <Col sm={{ size: 10, offset: 2 }}>
             <Button>Submit</Button>
           </Col>
         </FormGroup>
       </Form>
+      
       </Container>
     );
   }
