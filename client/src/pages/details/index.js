7b7f4fddc97413780e5aeb86ce87acc4c0f77996
mkumar3 <mkumar3@exponential.com>
2019-06-27 11:23:29 +0530

    details

--- client/src/pages/details/index.js
+++ client/src/pages/details/index.js
@@ -1,8 +1,47 @@
-import React from "react";
 
-function Details(props) {
-  console.log(props.location.state);
-  return <div className="Details">Details</div>;
+
+import React from 'react';
+import { Container,Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+export default class Details extends React.Component {
+  constructor(props){
+    super(props);
+    console.log(props.location);
+  }
+  render() {
+    return (
+      <Container>
+       <Form>
+        <FormGroup row>
+          <Label for="exampleEmail" sm={2}>Email</Label>
+          <Col sm={10}>
+            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+          </Col>
+        </FormGroup>
+       
+        <FormGroup row>
+          <Label for="exampleSelect" sm={2}>Select</Label>
+          <Col sm={10}>
+            <Input type="select" name="select" id="exampleSelect" />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="exampleFile" sm={2}>File</Label>
+          <Col sm={10}>
+            <Input type="file" name="file" id="exampleFile" />
+            <FormText color="muted">
+              This is some placeholder block-level help text for the above input.
+              It's a bit lighter and easily wraps to a new line.
+            </FormText>
+          </Col>
+        </FormGroup>
+        <FormGroup check row>
+          <Col sm={{ size: 10, offset: 2 }}>
+            <Button>Submit</Button>
+          </Col>
+        </FormGroup>
+      </Form>
+      </Container>
+    );
+  }
 }
-
-export default Details;
