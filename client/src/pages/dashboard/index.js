fe0b66ab5e7e077b76f971b2a5f503596a84aad4
Raoul George <rgeorge@atlassian.com>
2019-06-28 09:52:56 +0530

    add styling for dashboard

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,87 +1,88 @@
 import React from "react";
 import Map from "./../map";
-import { Card, Button, Container, Row, Col } from "reactstrap";
+import { Card, Table, Button, Container, Row, Col } from "reactstrap";
 
 function Dashboard(props) {
+  var dataArray = [];
+  function getTotal() {
+    return props.location.state.data.length;
+  }
+  function getPeopleBetWeenSlot(startTime, startDuration, Endduration) {
+    var tempArray = [],
+      totalLength,
+      FilterLength;
+    totalLength = props.location.state.data.length;
+    var mapData = props.location.state.data.filter(function(value) {
+      const time = value.Time_Slot.split(".");
+      const min = parseInt(time[0]);
+      const sec = parseInt(time[1]);
+      if (min === 4 && (sec >= startDuration && sec <= Endduration)) {
+        tempArray.push(value);
+      }
+      return !(min === 4 && (sec >= startDuration && sec <= Endduration));
+    });
 
-    var dataArray = [];
-    function getTotal(){
-      return props.location.state.data.length;
-    }
-    function getPeopleBetWeenSlot(startTime,startDuration,Endduration){
-      var tempArray = [],totalLength,FilterLength;
-      totalLength = props.location.state.data.length;
-      var mapData = props.location.state.data.filter(function(value){
-          const time = value.Time_Slot.split('.');
-            const min = parseInt(time[0]);
-            const sec = parseInt(time[1]);
-            if((min === 4 && (sec >= startDuration && sec <= Endduration ))){
-                tempArray.push(value);
-            }
-            return !((min === 4 && (sec >= startDuration && sec <= Endduration )));  
+    dataArray[startDuration + ":" + Endduration] = tempArray;
+    FilterLength = totalLength - mapData.length;
+    return { FilterLength, totalLength };
+  }
 
-      });
-      dataArray[startDuration+":"+Endduration] = tempArray;
-      props.location.state.data = mapData;
-      FilterLength = totalLength - mapData.length;
-      return <div>
-            {FilterLength}| {totalLength}
-      </div>;
-      
-    }
+  function showGraph(start, end) {
+    console.log(dataArray[start + ":" + end]);
+  }
 
-   function showGraph (start,end){
-      console.log(dataArray[start+":"+end]);
-   }
   return (
-  
-    <Container>
-    <h1> Dash board </h1>
-    <Card>
-      <div className="Dashboard">
-        <div className="container">
-           <Row>
-          <Col xs="4" sm="3">
-              <Button onClick={()=>showGraph(0,10)} outline color="primary">Time slot</Button>
-          </Col>
-          <Col xs="4" sm="6">4:00 to 4:10</Col>
-          <Col xs="4" sm="3">{getPeopleBetWeenSlot(4,0,10)}</Col>
-        </Row>
-         <Row>
-          <Col xs="4" sm="3">
-              <Button onClick={()=>showGraph(10,20)}  outline color="primary">Time slot</Button>
-          </Col>
-          <Col xs="4" sm="6">4:10 to 4:20</Col>
-          <Col xs="4" sm="3">{getPeopleBetWeenSlot(4,10,20)}</Col>
-        </Row>
-         <Row>
-          <Col xs="4" sm="3">
-              <Button onClick={()=>showGraph(20,30)} outline color="primary">Time slot</Button>
-          </Col>
-          <Col xs="4" sm="6">4:20 to 4:30</Col>
-          <Col xs="4" sm="3">{getPeopleBetWeenSlot(4,20,30)}</Col>
-        </Row>
-         <Row>
-          <Col xs="4" sm="3">
-              <Button onClick={()=>showGraph(30,40)} outline color="primary">Time slot</Button>
-          </Col>
-          <Col xs="4" sm="6">4:30 to 4:40</Col>
-          <Col xs="4" sm="3">{getPeopleBetWeenSlot(4,30,40)}</Col>
-        </Row>
-         <Row>
-         <Col xs="4" sm="3">
-              <Button onClick={()=>showGraph(40,50)} outline color="primary">Time slot</Button>
-          </Col>
-          <Col xs="4" sm="6">4:40 to 4:50</Col>
-          <Col xs="4" sm="3">{getPeopleBetWeenSlot(4,40,50)}</Col>
-        </Row>
+    <div className="Dashboard">
+      <Container>
+        <h1> Dashboard </h1>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Time slot</th>
+              <th>People</th>
+              <th>Remaining</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr onClick={() => showGraph(0, 10)}>
+              <th scope="row">1</th>
+              <td>4:00 to 4:10</td>
+              <td>{getPeopleBetWeenSlot(4, 0, 10).FilterLength}</td>
+              <td>{getPeopleBetWeenSlot(4, 0, 10).totalLength}</td>
+            </tr>
+            <tr onClick={() => showGraph(10, 20)}>
+              <th scope="row">2</th>
+              <td>4:10 to 4:20</td>
+              <td>{getPeopleBetWeenSlot(4, 10, 20).FilterLength}</td>
+              <td>{getPeopleBetWeenSlot(4, 10, 20).totalLength}</td>
+            </tr>
+            <tr onClick={() => showGraph(20, 30)}>
+              <th scope="row">3</th>
+              <td>4:20 to 4:30</td>
+              <td>{getPeopleBetWeenSlot(4, 20, 30).FilterLength}</td>
+              <td>{getPeopleBetWeenSlot(4, 20, 30).totalLength}</td>
+            </tr>
+            <tr onClick={() => showGraph(30, 40)}>
+              <th scope="row">4</th>
+              <td>4:30 to 4:40</td>
+              <td>{getPeopleBetWeenSlot(4, 30, 40).FilterLength}</td>
+              <td>{getPeopleBetWeenSlot(4, 30, 40).totalLength}</td>
+            </tr>
+            <tr onClick={() => showGraph(40, 50)}>
+              <th scope="row">4</th>
+              <td>4:40 to 4:50</td>
+              <td>{getPeopleBetWeenSlot(4, 40, 50).FilterLength}</td>
+              <td>{getPeopleBetWeenSlot(4, 40, 50).totalLength}</td>
+            </tr>
+          </tbody>
+        </Table>
+
+        <div className="map-container">
+          <Map loc={props.location.state.Location} />
         </div>
-        </div>
-      </Card>
-      <div className="map-container">
-        <Map loc={props.location.state.Location} />
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 }
 
